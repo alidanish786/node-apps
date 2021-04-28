cc0e8fd9dc84cd6e7f8cc6501c1e52b52410c0fe
alidanish786 <mohddanish47@gmail.com>
2021-04-29 02:01:08 +0530

    Correction-add app.js

--- app.js
+++
@@ -1,3 +0,0 @@
-const request=require('request')
-
-const url="http://api.weatherstack.com/current?access_key=f3212a27055c3e424f9062bb6bddbd0c&query=37.8267,-122.4233"
---
+++ weather-app/app.js
@@ -0,0 +1,8 @@
+const request=require('request')
+
+const url="http://api.weatherstack.com/current?access_key=f3212a27055c3e424f9062bb6bddbd0c&query=37.8267,-122.4233"
+
+request({url:url},(error,response)=>{
+    const data= JSON.parse(response.body)
+    console.log(data.current)
+})
