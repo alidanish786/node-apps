056096eedab96cefa5babced2163bba8a5682daf
alidanish786 <mohddanish47@gmail.com>
2021-04-29 19:45:51 +0530

    wheather-geocode changes

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,29 @@
 const request=require('request')
+const geoCode=require('./utils/geocode')
 
-const url="http://api.weatherstack.com/current?access_key=f3212a27055c3e424f9062bb6bddbd0c&query=37.8267,-122.4233"
+// const url="http://api.weatherstack.com/current?access_key=f3212a27055c3e424f9062bb6bddbd0c&query=&units=f"
 
-request({url:url},(error,response)=>{
-    const data= JSON.parse(response.body)
-    console.log(data.current)
+// request({url:url,json:true},(error,response)=>{
+//     //console.log(response.body.current)
+//     if(error){
+//         console.log('Unable to connect to weather service!')
+//     }
+//     else if(response.body.error){
+//         console.log('Unable to find location')
+//     }
+//     else{
+//         console.log('Current temp is '+resp.temperature+' degrees but feels like it is '+ resp.feelslike+' degrees')
+//     }
+       
+// })
+
+//Geo coding
+
+const mapBoxUrl="https://api.mapbox.com/geocoding/v5/mapbox.places/12undertaker.json?access_token=pk.eyJ1IjoiaGl0bWFuMjAwNiIsImEiOiJja28ybWN2ZjYwM2RnMndtdXd3cHN1OXg5In0.8VpR-poOVftYQB8X2C1arA&limit=1"
+
+geoCode('New Delhi',(error,data)=>{
+    debugger;
+    console.log('Error:-'+error)
+    console.log('Data:-'+data)
+    console.log('Latitude and longitue are here respectively- ' +data.latitude +' and '+data.longitude+' and location is '+data.location)
 })
