1ebc97014a01a522b06bb96a154f053ed9ab36f4
alidanish786 <mohddanish47@gmail.com>
2021-05-01 19:05:46 +0530

    Dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,32 +1,42 @@
-const express=require('express')
-const path=require('path')
+const path = require('path')
+const express = require('express')
 
-const app=express()
+const app = express()
+//const publicDirectoryPath = path.join(__dirname, '../public')
 
-const directory=path.join(__dirname,'../public')
+const viewsDirectoryPath = path.join(__dirname, '../views')
+app.set('views', path.join(viewsDirectoryPath))
+app.set('view engine', 'hbs')
 
-app.use(express.static(directory))
+// app.use(express.static(publicDirectoryPath))
 
-// app.get('',(req,res)=>{
-//     res.send('Hi Express')
-// })
-
-// app.get('/help',(req,res)=>{
-//     res.send('Hi Express help')
-// })
-
-// app.get('/about',(req,res)=>{
-//     res.send('<h1>About</h1>')
-// })
-
-app.get('/wheather',(req,res)=>{
-    res.send({
-        forecast:'30 degrees',
-        location:'Delhi'
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
     })
 })
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
 
-app.listen(3000,()=>{
-    console.log('Server is started at 3000 port')
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
